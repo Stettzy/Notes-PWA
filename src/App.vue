894beb56c207e16dfc3c4303fead857390b1b24a
Stettzy <{ID}+{username}@users.noreply.github.com>
2023-10-16 15:45:02 +0200

    added serviceWorker; edit/update & save to sharedDB rewamp;

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,52 @@
 			<!-- sidebar -->
 			<div class="h-0 overflow-auto flex-grow">
 				<div class="mt-4">
-					<a 
-						class="flex items-center h-8 text-sm pl-8 pr-3"
-						href="#"
-						v-for="note in notes"
-						:key="note.created"	
-					>
+					<div class="flex items-center justify-between h-8 text-sm pl-8 pr-3 ml-2">
+						<button @click="this.activeNote = {}">All notes</button>
+						<button 
+							class="text-center border border-gray-500 pl-3 pr-3 mr-2"
+							@click="newNote"
+						>
+							+
+						</button>
+					</div>
+					<a v-for="note in notes" :key="note.created" @click.prevent="openNote(note)"
+						class="flex items-center h-8 text-sm pl-8 pr-3" href="#">
 						<span class="ml-2 leading-none">{{ new Date(note.created).toLocaleString() }}</span>
 					</a>
 				</div>
 			</div>
 		</div>
-		<div class="flex flex-col flex-grow">
+
+		<div
+			class="flex flex-col flex-grow"
+			v-if="Object.keys(activeNote).length"
+		>
 			<!-- main content -->
 			<div class="flex flex-col flex-grow overflow-auto">
-				<editor-content :editor="editor"/>
+				<editor-content :editor="editor" />
 			</div>
 			<div class="flex justify-end items-center h-16 bg-gray-100 border-t border-gray-300 text-right px-8">
-				<button @click="saveNote" class="border border-gray-500 py-2 px-6 rounded hover:bg-white">Save Note</button>
+				<button class="border border-gray-500 py-2 px-6 rounded hover:bg-white" @click="saveNote">
+					Save Note
+				</button>
+			</div>
+		</div>
+		<div 
+			class="prose flex flex-col flex-grow"
+			v-else
+		>
+			<div v-for="note in notes" :key="note.created">
+				<h1>{{ note.created }}</h1>
+				<div v-html="note.content"></div>
+				<hr>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import {Editor, EditorContent} from '@tiptap/vue-3';
+import { Editor, EditorContent } from '@tiptap/vue-3';
 import StarterKit from '@tiptap/starter-kit';
 
 export default {
@@ -41,7 +62,8 @@ export default {
 		return {
 			editor: null,
 			database: null,
-			notes: []
+			notes: [],
+			activeNote: {}
 		}
 	},
 	async created() {
@@ -68,8 +90,8 @@ export default {
 	methods: {
 		async getDatabase() {
 			return new Promise((resolve, reject) => {
-				let db = window.indexedDB.open('notes', 1)
-				
+				let db = window.indexedDB.open('notes')
+
 				db.onerror = e => {
 					reject(e.target.result, "Error opening database")
 				};
@@ -79,13 +101,59 @@ export default {
 				}
 
 				db.onupgradeneeded = e => {
-					e.target.result.deleteObjectStore('notes');
-					e.target.result.createObjectStore('notes', {keyPath: "created"});
+					// e.target.result.deleteObjectStore('notes');
+					e.target.result.createObjectStore('notes', { keyPath: "created" });
 				}
 			})
 		},
 		async saveNote() {
 			console.log('fired')
+			return new Promise((resolve, reject) => {
+				let noteStore = this.database.transaction('notes', 'readwrite')
+					.objectStore('notes');
+				
+				let noteRequest = noteStore.get(this.activeNote.created);
+
+				noteRequest.onerror = e => {
+					reject('Error saving the note in the database', e.target.result)
+				}
+				
+				noteRequest.onsuccess = e => {
+					let note = e.target.result;
+					note.content = this.editor.getHTML();
+
+					let updateRequest = noteStore.put(note);
+
+					updateRequest.onerror = e => {
+						reject('Error storing the updated note in the database', e.target.result)
+					}
+
+					updateRequest.onsuccess = e => {
+						let noteIndex = this.notes.findIndex(n => n.created === note.created);
+						this.notes[noteIndex] = note;
+						resolve(e.target.result);
+					}
+				}
+			})
+		},
+		async getNotes() {
+			console.log('notes fired');
+			return new Promise((resolve) => {
+				this.database.transaction('notes')
+					.objectStore('notes')
+					.getAll()
+					.onsuccess = e => {
+						console.log("ALO", e.target.result);
+						resolve(e.target.result)
+					};
+			})
+		},
+		openNote(note) {
+			this.editor.commands.setContent(note.content);
+			this.activeNote = note;
+		},
+
+		newNote() {
 			return new Promise((resolve) => {
 				let transaction = this.database.transaction('notes', 'readwrite');
 
@@ -93,33 +161,20 @@ export default {
 					resolve(e.target.result);
 				}
 
-				let now = new Date();
-
+				let date = new Date();
 				let note = {
-					content: this.editor.getHTML(),
-					created: now.getTime()
+					created: date.getTime(),
+					content: ''
 				}
-
+	
 				this.notes.unshift(note);
 
-				transaction.objectStore('notes').add({
-					content: this.editor.getHTML(),
-					created: now.getTime()
-				})
+				transaction.objectStore('notes').add(note)
+				
 			})
-		},
-		async getNotes() {
-			console.log('notes fired');
-			return new Promise((resolve) => {
-				this.database.transaction('notes')
-				.objectStore('notes')
-				.getAll()
-				.onsuccess = e => {
-					console.log("ALO", e.target.result);
-					resolve(e.target.result)
-				};
-			})
+
 		}
+
 	}
 };
 </script>
